924f529bfeb0410d4264221a0ed66e542f3d991c
Aaron Rajoo <aaronrajoo@gmail.com>
2026-05-13 23:12:03 +0800

    Fix anger card filename casing

--- data/cards.ts
+++ data/cards.ts
@@ -14,7 +14,7 @@ export const cards: GameCard[] = [
     type: "emotion",
     theme: "Anger",
     title: "Anger",
-    image: "/cards/emotions/temp_emotion_anger.png",
+    image: "/cards/emotions/emotion_anger.png",
   },
   {
     id: "emotion_ambition",
